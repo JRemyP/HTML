ce78d29ad284f74af07a6562d93fdee732215919
JRemyP <jremy@ac3-studio.com>
2024-02-29 17:41:29 +0100

    second

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 document.writeln("This is a JS code");
 
-const screenLength = document.getElementById('lenght-screen');
+const screenlenght = document.getElementById('lenght-screen');
 const screenWidth = document.getElementById('width-screen');
 const buttonValid = document.getElementById('valid');
 const pitchModule = document.getElementById('pitch-module');
+const widthModule = document.getElementById('width-module');
+const lenghtModule = document.getElementById('lenght-module');
+
+const xModule = document.getElementById('x-module');
+const yModule = document.getElementById('y-module');
 
 const output = document.getElementById('output');
 
+
+/* VARIABLES */
+var module = {
+    x:0,
+    y:0,
+    width:0,
+    lenght:0,
+    p:0
+}
+
+var screen = {
+    x:0,
+    y:0,
+    width:0,
+    lenght:0,
+    p:0.0
+}
 
 function displayValue(){
     let i = parseInt(screenWidth.value);
@@ -16,6 +38,22 @@ function displayValue(){
     output.innerHTML = HTMLString;
 }
 
+function displayQtyPixels(){
+
+    module.width = parseInt(widthModule.value);
+    module.lenght = parseInt(lenghtModule.value);
+    let pStr = `${pitchModule.value}`;
+    pStr = pStr.replace('p','');
+    pStr = pStr.slice(0, 1) + "." + pStr.slice(1);
+    module.p = parseFloat(pStr);
+
+    module.x = ~~(module.width / module.p);
+    module.y = ~~(module.lenght / module.p);
+
+    xModule.innerHTML = module.x;
+    yModule.innerHTML = module.y;
+
+}
 
 function displayPitch(){
     HTMLString=`
@@ -25,4 +63,7 @@ function displayPitch(){
 }
 
 screenWidth.addEventListener('change', displayValue);
-buttonValid.addEventListener('click', displayPitch);
+
+pitchModule.addEventListener('change', displayQtyPixels);
+widthModule.addEventListener('change', displayQtyPixels);
+lenghtModule.addEventListener('change', displayQtyPixels);

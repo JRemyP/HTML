b3c96b9990cf3cb3393b673dd7406fbcc834f6b1
JRemyP <jremy@ac3-studio.com>
2024-03-04 15:41:56 +0100

    maj

--- script.js
+++ script.js
@@ -1,59 +1,68 @@
-document.writeln("This is a JS code");
 
-const screenlenght = document.getElementById('lenght-screen');
-const screenWidth = document.getElementById('width-screen');
-const buttonValid = document.getElementById('valid');
-const pitchModule = document.getElementById('pitch-module');
-const widthModule = document.getElementById('width-module');
-const lenghtModule = document.getElementById('lenght-module');
-
-const xModule = document.getElementById('x-module');
-const yModule = document.getElementById('y-module');
+const wantedScreenElements = document.querySelector('#wanted-screen');
+const moduleElements = document.querySelector('#module');
+const realScreenElements = document.querySelector('#real');
 
 const output = document.getElementById('output');
+const buttonValid = document.getElementById('valid');
 
+var wantedScreen = {
+    lenghtElement : wantedScreenElements.querySelector('#lenght'),
+    widthElement : wantedScreenElements.querySelector('#width'),
+    validElement : wantedScreenElements.querySelector('#valid'),
+    lenght : 0,
+    width : 0,
+}
+
+var realScreen = {
+    lenght : 0,
+    width : 0,
+    area : 0,
+
+    xRes: 0,
+    yRes: 0,
+    totalRes : 0,
+
+    xModule: 0,
+    yModule: 0,
+    totalModule : 0,
+
+
+    widthElement : realScreenElements.querySelector('#width'),
+    lenghtElement : realScreenElements.querySelector('#lenght'),
+    areaElement : realScreenElements.querySelector('#area'),
+    xResElement : realScreenElements.querySelector('#x-res'),
+    yResElement : realScreenElements.querySelector('#y-res'),
+    totalResElement : realScreenElements.querySelector('#total-res'),
+    xModuleElement : realScreenElements.querySelector('#x-module'),
+    yModuleElement : realScreenElements.querySelector('#y-module'),
+    totalModuleElement : realScreenElements.querySelector('#total-module'),
+}
 
 /* VARIABLES */
 var module = {
     x:0,
     y:0,
+    p:0,
     width:0,
     lenght:0,
-    p:0
+    widthElement: moduleElements.querySelector('#width'),
+    lenghtElement: moduleElements.querySelector('#lenght'),
+    pElement: moduleElements.querySelector('#pitch'),
+    xElement: moduleElements.querySelector('#x'),
+    yElement: moduleElements.querySelector('#y'),
 }
 
-var screen = {
-    x:0,
-    y:0,
-    width:0,
-    lenght:0,
-    p:0.0
-}
+/*function console(m){
 
-function displayValue(){
-    let i = parseInt(screenWidth.value);
-    HTMLString=`
-    <p>Width : ${i}</p>
-    `
-    output.innerHTML = HTMLString;
-}
+    let i = parseInt(m.value);
+    if(isNaN(i)){
+        i = parseInt(m); 
+    }
+    HTMLString=`<p>- ${m} : ${i}</p>`
+    output.insertAdjacentHTML("afterbegin", HTMLString);
 
-function displayQtyPixels(){
-
-    module.width = parseInt(widthModule.value);
-    module.lenght = parseInt(lenghtModule.value);
-    let pStr = `${pitchModule.value}`;
-    pStr = pStr.replace('p','');
-    pStr = pStr.slice(0, 1) + "." + pStr.slice(1);
-    module.p = parseFloat(pStr);
-
-    module.x = ~~(module.width / module.p);
-    module.y = ~~(module.lenght / module.p);
-
-    xModule.innerHTML = module.x;
-    yModule.innerHTML = module.y;
-
-}
+}*/
 
 function displayPitch(){
     HTMLString=`
@@ -62,8 +71,95 @@ function displayPitch(){
     output.innerHTML = HTMLString;
 }
 
-screenWidth.addEventListener('change', displayValue);
+function calculateTotalScreen(event){
+    event.preventDefault();
 
-pitchModule.addEventListener('change', displayQtyPixels);
-widthModule.addEventListener('change', displayQtyPixels);
-lenghtModule.addEventListener('change', displayQtyPixels);
+    
+
+    module.width = parseInt(module.widthElement.value);
+    module.lenght = parseInt(module.lenghtElement.value);
+    let pStr = `${module.pElement.value}`;
+    pStr = pStr.replace('p','');
+    pStr = pStr.slice(0, 1) + "." + pStr.slice(1);
+    module.p = parseFloat(pStr);
+
+    if(module.p == 0 || module.lenght == 0 || module.width == 0){
+        return ;
+    }
+
+    module.x = ~~(module.lenght / module.p);
+    module.y = ~~(module.width / module.p);
+
+    module.xElement.innerHTML = module.x;
+    module.yElement.innerHTML = module.y;
+
+
+    wantedScreen.lenght = parseInt(wantedScreen.lenghtElement.value);
+    realScreen.xModule = Math.ceil(wantedScreen.lenght / module.lenght);
+    realScreen.xModuleElement.innerHTML = realScreen.xModule;
+
+    wantedScreen.width= parseInt(wantedScreen.widthElement.value);
+    realScreen.yModule = Math.ceil(wantedScreen.width / module.width);
+    realScreen.yModuleElement.innerHTML = realScreen.yModule;
+
+    realScreen.totalModule = realScreen.yModule * realScreen.xModule ;
+    realScreen.totalModuleElement.innerHTML = realScreen.totalModule;
+
+    realScreen.lenght = realScreen.xModule * module.lenght;
+    realScreen.width = realScreen.yModule * module.width;
+    realScreen.widthElement.innerHTML = realScreen.width;
+    realScreen.lenghtElement.innerHTML = realScreen.lenght;
+
+    realScreen.area = (realScreen.lenght/1000.0) * (realScreen.width/1000.0);
+    realScreen.areaElement.innerHTML = realScreen.area;
+
+
+    realScreen.xRes = realScreen.xModule * module.x;
+    realScreen.yRes = realScreen.yModule * module.y;
+    realScreen.xResElement.innerHTML = realScreen.xRes;
+    realScreen.yResElement.innerHTML = realScreen.yRes;
+
+    realScreen.totalRes = realScreen.xRes * realScreen.yRes;
+    realScreen.totalResElement.innerHTML = realScreen.totalRes;
+
+
+
+    /*console.log("wantedScreen.lenght : " + wantedScreen.lenght);
+    console.log("wantedScreen.width : " +wantedScreen.width);
+
+    console.log("module.lenght : " +module.lenght);
+    console.log("module.width : " +module.width);
+    console.log("module.x : " +module.x);
+    console.log("module.y : " +module.y);
+    console.log("module.p : " +module.p);
+
+    console.log("realScreen.lenght : " +realScreen.lenght);
+    console.log("realScreen.width : " +realScreen.width);
+    console.log("realScreen.xRes : " +realScreen.xRes);
+    console.log("realScreen.yRes : " +realScreen.yRes);
+    console.log("realScreen.xModule : " +realScreen.xModule);
+    console.log("realScreen.yModule : " +realScreen.yModule);*/
+
+}
+
+
+function hideModule(){
+    wantedScreen.widthElement.removeAttribute("readonly");
+    wantedScreen.widthElement.value="";
+    wantedScreen.lenghtElement.removeAttribute("readonly");
+    wantedScreen.lenghtElement.value="";
+    wantedScreen.validElement.innerHTML = "Valid";
+    moduleElements.classList.add('hide'); 
+}
+
+function revealModule(){
+    wantedScreen.widthElement.setAttribute("readonly", true);
+    wantedScreen.lenghtElement.setAttribute("readonly", true);
+    wantedScreen.validElement.innerHTML = "Clear";
+    moduleElements.classList.remove('hide'); 
+}
+
+moduleElements.addEventListener('submit', calculateTotalScreen);
+
+//screenWidth.addEventListener('change', console);
+

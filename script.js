378f88a3b5a4a6074f41bdca083cbf41b547524d
JRemyP <jremy@ac3-studio.com>
2024-03-13 17:30:45 +0100

    update JS code to batter running, using array functions

--- script.js
+++ script.js
@@ -1,87 +1,201 @@
+const pitchSelectElement = document.getElementById('pitch-select');
+const sizeSelectElement = document.getElementById('size-select');
+const infosModuleElement = document.getElementById('infos-module');
+const infosScreenElement = document.getElementById('real');
+const lengthInputElement = document.getElementById('length');
+const widthInputElement = document.getElementById('width');
+const pricingInputElement = document.getElementById('pricing');
 
-const wantedScreenElements = document.getElementById('wanted-screen');
-const moduleElements = document.getElementById('module');
-const realScreenElements = document.getElementById('real');
-const priceElements = document.getElementById('pricing');
+const moduleData = [
+    { 
+        pitchName : "P1.25",
+        pitch : 1.25,
+        size : [
+            {length : 320, width : 160, x : 256, y : 128, price : 75},
+            {length : 240, width : 120, x : 192, y : 96, price : 55},
+        ]      
+    },
+    { 
+        pitchName : "P1.29",
+        pitch : 1.29,
+        size : [
+            {length : 320, width : 160, x : 248, y : 124, price : 75},
+        ]      
+    },
+    { 
+        pitchName : "P1.53", 
+        pitch : 1.538461538461538,
+        size : [
+            {length : 320, width : 160, x : 120, y : 100, price : 55},
+            {length : 240, width : 120, x : 100, y : 80, price : 65},
+        ]      
+    },
+    { 
+        pitchName : "P1.56", 
+        pitch : 1.56,
+        size : [
+            {length : 320, width : 160, x : 204, y : 102, price : 58},
+        ]      
+    },
+    { 
+        pitchName : "P1.579", 
+        pitch : 1.579,
+        size : [
+            {length : 320, width : 160, x : 152, y : 76, price : 85},
+        ]      
+    },
+    { 
+        pitchName : "P1.667", 
+        pitch : 1.6666666666666667,
+        size : [
+            {length : 240, width : 120, x : 144, y : 72, price : 54},
+        ]      
+    },
+    { 
+        pitchName : "P2.00", 
+        pitch : 2.0,
+        size : [
+            {length : 360, width : 160, x : 160, y : 80, price : 55},
+            {length : 240, width : 120, x : 120, y : 60, price : 65},
+        ]      
+    },
+    { 
+        pitchName : "P2.50", 
+        pitch : 2.50,
+        size : [
+            {length : 320, width : 160, x : 128, y : 64, price : 55},
+            {length : 240, width : 120, x : 96, y : 48, price : 65},
+        ]      
+    },
+    { 
+        pitchName : "P3.00", 
+        pitch : 3.00,
+        size : [
+            {length : 320, width : 160, x : 104, y : 52, price : 55},
+            {length : 240, width : 120, x : 80, y : 40, price : 65},
+        ]      
+    },
+    { 
+        pitchName : "P4.00", 
+        pitch : 4.00,
+        size : [
+            {length : 320, width : 160, x : 80, y : 40, price : 55},
+            {length : 240, width : 120, x : 60, y : 30, price : 65},
+        ]      
+    },
+    { 
+        pitchName : "P5.00", 
+        pitch : 5.00,
+        size : [
+            {length : 320, width : 160, x : 64, y : 32, price : 55},
+        ]
+    },
+];
 
-const output = document.getElementById('output');
-const buttonValid = document.getElementById('valid');
+const setPitchSelectOption = () => {
+    pitchSelectElement.innerHTML = moduleData.map( e => {
+        return `<option value="${e.pitch}">${e.pitchName}</option>`;
+    }).join('');
+};
 
-
-
-var wantedScreen = {
-    lenghtElement : wantedScreenElements.querySelector('#lenght'),
-    widthElement : wantedScreenElements.querySelector('#width'),
-    validElement : wantedScreenElements.querySelector('#valid'),
-    lenght : 0,
-    width : 0,
+const setModuleSelectOption = (currentModule) => {
+    sizeSelectElement.innerHTML = currentModule.size.map( e => {
+        return `<option value="${e.length}">${e.length} x ${e.width}</option><p>mm</p>`;
+    }).join('');
 }
 
-var realScreen = {
-    lenght : 0,
-    width : 0,
-    area : 0,
+const displayInfosModule = (currentSizeModule) => {
+    infosModuleElement.innerHTML = `
+    <p>X : ${currentSizeModule.x}</p>
+    <p>Y : ${currentSizeModule.y}</p>
+    <p>Price : ${currentSizeModule.price} €</p>
+    `;
+};
 
-    xRes: 0,
-    yRes: 0,
-    totalRes : 0,
+const displayInfosScreen = () => {
 
-    xModule: 0,
-    yModule: 0,
-    totalModule : 0,
+displayInfosModule(moduleData[pitchSelectElement.selectedIndex].size[sizeSelectElement.selectedIndex]);
 
-    vpuOutputs : 0,
+const currentPitch = pitchSelectElement.selectedIndex;
+const currentSize = sizeSelectElement.selectedIndex;
 
-    widthElement : realScreenElements.querySelector('#width'),
-    lenghtElement : realScreenElements.querySelector('#lenght'),
-    areaElement : realScreenElements.querySelector('#area'),
+const lengthInput = parseInt(lengthInputElement.value);
+const widthInput = parseInt(widthInputElement.value);
 
-    xResElement : realScreenElements.querySelector('#x-res'),
-    yResElement : realScreenElements.querySelector('#y-res'),
-    totalResElement : realScreenElements.querySelector('#total-res'),
+const xModuleQty = Math.ceil(lengthInput / moduleData[currentPitch].size[currentSize].length);
+const yModuleQty = Math.ceil(widthInput / moduleData[currentPitch].size[currentSize].width);
+const totalModuleQty = xModuleQty * yModuleQty;
 
-    xModuleElement : realScreenElements.querySelector('#x-module'),
-    yModuleElement : realScreenElements.querySelector('#y-module'),
-    totalModuleElement : realScreenElements.querySelector('#total-module'),
+const lengthReal = xModuleQty * moduleData[currentPitch].size[currentSize].length;
+const widthReal = yModuleQty * moduleData[currentPitch].size[currentSize].width;
+const area = (lengthReal/1000.0) * (widthReal/1000.0);
 
-    vpuOutputsElement : realScreenElements.querySelector('#vpu-outputs'),
-}
+const xRes = xModuleQty * moduleData[currentPitch].size[currentSize].x;
+const yRes = yModuleQty * moduleData[currentPitch].size[currentSize].y;
+const totalRes = xRes * yRes;
 
-/* VARIABLES */
-var module = {
-    x:0,
-    y:0,
-    p:0,
-    width:0,
-    lenght:0,
-    price: 0,
-    widthElement: moduleElements.querySelector('#width'),
-    lenghtElement: moduleElements.querySelector('#lenght'),
-    pElement: moduleElements.querySelector('#pitch'),
-    xElement: moduleElements.querySelector('#x'),
-    yElement: moduleElements.querySelector('#y'),
-    priceElement: moduleElements.querySelector('#price'),
-}
+const vpuOutputs = Math.ceil(totalRes / 650000.0);
 
-var price = {
-    total : 0,
-    area : 0,
-    totalElement : priceElements.querySelector('#total'),
-    areaElement : priceElements.querySelector('#m2'),
-}
+const totalPrice = moduleData[currentPitch].size[currentSize].price * totalModuleQty;
+const areaPrice =  totalPrice / area;
 
 
-function displayPitch(){
-    HTMLString=`
-    <p>Pitch : ${pitchModule.value}</p>
-    `
-    output.innerHTML = HTMLString;
-}
+const vignettesInfos = [
+    `<h2>Dimensions</h2>
+    <p>length : ${lengthReal.toLocaleString()} mm</p>
+    <p>width : ${widthReal.toLocaleString()} mm</p>
+    <p>Area : ${area.toFixed(2)} m²</p>
+    `,
+    `<h2>Resolution</h2>
+    <p>X : ${xRes} px</p>
+    <p>Y : ${yRes} px</p>
+    <p>Total : ${totalRes.toLocaleString()} px</p>
+    `,
+    `<h2>Module quantity</h2>
+    <p>X : ${xModuleQty}</p>
+    <p>Y : ${yModuleQty}</p>
+    <p>Total : ${totalModuleQty}</p>
+    `,
+    `<h2>VPU dimensions</h2>
+    <p>outputs RJ45 quantity : ${vpuOutputs}</p>`
+   ];
+
+   infosScreenElement.innerHTML = '<legend>Real Screen</legend>';
+   infosScreenElement.innerHTML += vignettesInfos.map( (el) => '<div class="vignette">'+el+'</div>');
+
+   pricingInputElement.innerHTML =`
+   <legend>Pricing</legend>
+   <p>Total : ${totalPrice.toLocaleString()} €</p>
+   <p>By m² : ${areaPrice.toFixed(2)} €</p>
+   `;
+};
+
+setPitchSelectOption();
+setModuleSelectOption(moduleData[0]);
+displayInfosScreen();
+
+pitchSelectElement.addEventListener('change', () =>  setModuleSelectOption(moduleData[pitchSelectElement.selectedIndex]) );
+pitchSelectElement.addEventListener('change', displayInfosScreen);
+sizeSelectElement.addEventListener('change', displayInfosScreen);
+lengthInputElement.addEventListener('change', displayInfosScreen);
+widthInputElement.addEventListener('change', displayInfosScreen);
+
+
+
+
+
+
+
+
+
+
+
+
 
 function calculateTotalScreen(){
 
     module.width = parseInt(module.widthElement.value);
-    module.lenght = parseInt(module.lenghtElement.value);
+    module.length = parseInt(module.lengthElement.value);
     module.price = module.priceElement.value;
 
     let pStr = `${module.pElement.value}`;
@@ -89,19 +203,19 @@ function calculateTotalScreen(){
     pStr = pStr.slice(0, 1) + "." + pStr.slice(1);
     module.p = parseFloat(pStr);
 
-    if(module.p == 0 || module.lenght == 0 || module.width == 0){
+    if(module.p == 0 || module.length == 0 || module.width == 0){
         return ;
     }
 
-    module.x = ~~(module.lenght / module.p);
+    module.x = ~~(module.length / module.p);
     module.y = ~~(module.width / module.p);
 
     module.xElement.innerHTML = module.x;
     module.yElement.innerHTML = module.y;
 
 
-    wantedScreen.lenght = parseInt(wantedScreen.lenghtElement.value);
-    realScreen.xModule = Math.ceil(wantedScreen.lenght / module.lenght);
+    wantedScreen.length = parseInt(wantedScreen.lengthElement.value);
+    realScreen.xModule = Math.ceil(wantedScreen.length / module.length);
     realScreen.xModuleElement.innerHTML = realScreen.xModule;
 
     wantedScreen.width= parseInt(wantedScreen.widthElement.value);
@@ -111,12 +225,12 @@ function calculateTotalScreen(){
     realScreen.totalModule = realScreen.yModule * realScreen.xModule ;
     realScreen.totalModuleElement.innerHTML = realScreen.totalModule;
 
-    realScreen.lenght = realScreen.xModule * module.lenght;
+    realScreen.length = realScreen.xModule * module.length;
     realScreen.width = realScreen.yModule * module.width;
     realScreen.widthElement.innerHTML = realScreen.width.toLocaleString();
-    realScreen.lenghtElement.innerHTML = realScreen.lenght.toLocaleString();
+    realScreen.lengthElement.innerHTML = realScreen.length.toLocaleString();
 
-    realScreen.area = (realScreen.lenght/1000.0) * (realScreen.width/1000.0);
+    realScreen.area = (realScreen.length/1000.0) * (realScreen.width/1000.0);
     realScreen.areaElement.innerHTML = realScreen.area.toFixed(3);
 
 
@@ -138,7 +252,3 @@ function calculateTotalScreen(){
     price.areaElement.innerHTML = price.area.toFixed(2) + " €";
 
 }
-
-moduleElements.addEventListener('change', calculateTotalScreen);
-wantedScreenElements.addEventListener('change', calculateTotalScreen);
-//screenWidth.addEventListener('change', console);
